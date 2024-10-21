7232a08eddfdbd7d523f3e9c8254499dbb1e995b
dev-timm <timm.kuemmeth@gmail.com>
2024-10-21 13:07:16 +0200

    set up basic express server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello World!!!!');
+const express = require('express');
+const app = express();
+
+app.get('/mentor', (req, res) => {
+  res.send('MentorUp');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}...`);
+});
