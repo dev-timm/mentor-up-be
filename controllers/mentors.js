1fd78ed83b5c01769c3384ed92636fcd56a92e5d
dev-timm <timm.kuemmeth@gmail.com>
2024-10-28 12:51:45 +0100

    add pagination when displaying list of mentors

--- controllers/mentors.js
+++ controllers/mentors.js
@@ -19,7 +19,6 @@ const getAllMentors = asyncWrapper(async (req, res) => {
   let result = Mentor.find(queryObject);
 
   // sort mentor list
-
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
@@ -28,11 +27,17 @@ const getAllMentors = asyncWrapper(async (req, res) => {
   }
 
   // show only specific mentor info
-
   if (fields) {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  // paginate mentor results
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 8;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const mentors = await result;
   res.status(200).json({ mentors, nbHits: mentors.length });
