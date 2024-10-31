9665e8ec1be3407b8a6eb3f422a64481b6794abd
dev-timm <timm.kuemmeth@gmail.com>
2024-10-31 13:22:57 +0100

    add email field to mentor model

--- models/Mentor.js
+++ models/Mentor.js
@@ -8,6 +8,14 @@ const MentorSchema = new mongoose.Schema({
     minlegth: [4, 'name can not be less than 4 characters long'],
     maxlength: [30, 'name can not be more than 30 characters long'],
   },
+  email: {
+    type: String,
+    required: [true, 'please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide valid email',
+    ],
+  },
   profession: {
     type: String,
     required: [true, 'please provide profession'],
