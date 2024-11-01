5544fa63e60934e403163cd35b0514ab0261cde4
dev-timm <timm.kuemmeth@gmail.com>
2024-11-01 13:10:53 +0100

    remove email and password from mentor model and move to new user model

--- models/Mentor.js
+++ models/Mentor.js
@@ -8,20 +8,6 @@ const MentorSchema = new mongoose.Schema({
     minlegth: [4, 'name can not be less than 4 characters long'],
     maxlength: [30, 'name can not be more than 30 characters long'],
   },
-  email: {
-    type: String,
-    required: [true, 'please provide email'],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please provide valid email',
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'please provide password'],
-    minlegth: [4, 'password can not be less than 4 characters long'],
-  },
   profession: {
     type: String,
     required: [true, 'please provide profession'],
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'please provide name'],
+    trim: true,
+    minlegth: [4, 'name can not be less than 4 characters long'],
+    maxlength: [30, 'name can not be more than 30 characters long'],
+  },
+  email: {
+    type: String,
+    required: [true, 'please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide valid email',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'please provide password'],
+    minlegth: [4, 'password can not be less than 4 characters long'],
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
