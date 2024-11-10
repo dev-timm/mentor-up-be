6479ddbf1df5f20faf1387057413e2d62db28b6c
dev-timm <timm.kuemmeth@gmail.com>
2024-11-10 13:04:17 +0100

    enhance security of api

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+require('dotenv').config();
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -6,12 +13,20 @@ const auth = require('./routes/auth');
 
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
-require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 
 // routes
 app.use('/api/v1/auth', auth);
