c6b8703a21dc9919ff960dacc259939b598af386
dev-timm <timm.kuemmeth@gmail.com>
2024-11-13 15:10:55 +0100

    add functionality to get all users and single user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,20 @@
+const User = require('../models/User');
 const asyncWrapper = require('../middleware/async');
+const { StatusCodes } = require('http-status-codes');
+const customError = require('../errors');
 
 const getAllUsers = asyncWrapper(async (req, res) => {
-  res.send('get all users');
+  const users = await User.find({ role: 'user' }).select('-password');
+  res.status(StatusCodes.OK).json({ users });
 });
 
 const getSingleUser = asyncWrapper(async (req, res) => {
-  res.send('get single user');
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+  if (!user) {
+    return next(new NotFoundError(`No user with id: ${req.params.id}`));
+  }
+
+  res.status(StatusCodes.OK).json({ user });
 });
 
 const ShowCurrentUser = asyncWrapper(async (req, res) => {
