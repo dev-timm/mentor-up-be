3cafca4ae8aaf0e4fcd35374c4d1ed5b485cbed6
dev-timm <timm.kuemmeth@gmail.com>
2024-11-19 15:06:34 +0100

    add feature to create reviews for mentors

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,7 +1,37 @@
+const Review = require('../models/Review');
+const Mentor = require('../models/Mentor');
+const { StatusCodes } = require('http-status-codes');
+const customError = require('../errors');
+const { checkPermissions } = require('../utils');
+
 const asyncWrapper = require('../middleware/async');
 
 const createReview = asyncWrapper(async (req, res) => {
-  res.send('create review');
+  const { mentor: mentorId } = req.body;
+
+  const isValidMentor = await Mentor.findOne({ _id: mentorId });
+
+  if (!isValidMentor) {
+    return next(new NotFoundError(`No mentor with id: ${req.params.id}`));
+  }
+
+  const alreadySubmitted = await Mentor.findOne({
+    mentor: mentorId,
+    user: req.user.userId,
+  });
+
+  if (alreadySubmitted) {
+    return next(
+      new customError.BadRequestError(
+        'Already submitted review for this mentor'
+      )
+    );
+  }
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ review });
 });
 
 const getAllReviews = asyncWrapper(async (req, res) => {
